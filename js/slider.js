2a697488cc84eb3cca050cfada39203c07deae5c
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-24 15:29:26 +0200

    add slide markup

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,31 @@
-// console.log('slider')
-
 const slides = [
-	'./img/01.jpg',
-	'./img/02.jpg',
-	'./img/03.jpg',
-	'./img/04.jpg',
-	'./img/05.jpg',
+	{
+		imgSrc: './img/01.jpg',
+		isActive: false
+	},
+	{
+		imgSrc: './img/02.jpg',
+		isActive: false
+	},
+	{
+		imgSrc: './img/03.jpg',
+		isActive: true
+	},
+	{
+		imgSrc: './img/04.jpg',
+		isActive: false
+	},
+	{
+		imgSrc: './img/05.jpg',
+		isActive: false
+	}
 ]
+
+const app = new Vue({
+	el: '#app',
+	data: {
+		slides: slides
+	}
+});
+
+console.log(app.slides);
