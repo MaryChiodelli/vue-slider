33415505ed7b6aa1f34cc2be16b0e1b026554ed1
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-25 08:01:43 +0200

    refactoring

--- js/slider.js
+++ js/slider.js
@@ -1,58 +1,31 @@
 const slides = [
-	{
-		imgSrc: './img/01.jpg',
-		isActive: true
-	},
-	{
-		imgSrc: './img/02.jpg',
-		isActive: false
-	},
-	{
-		imgSrc: './img/03.jpg',
-		isActive: false
-	},
-	{
-		imgSrc: './img/04.jpg',
-		isActive: false
-	},
-	{
-		imgSrc: './img/05.jpg',
-		isActive: false
-	}
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg'
 ]
 
 const app = new Vue({
 	el: '#app',
 	data: {
-		slides: slides,
-		index: 0
+		slides,
+		activeIndex: 0
 	},
 	methods: {
 		goPrevSlide() {
-			const slideActive = slides[this.index];
-			slideActive.isActive = false;
-			
-			if (this.index === 0) {
-				this.index = this.slides.length - 1;
+			if (this.activeIndex === 0) {
+				this.activeIndex = this.slides.length - 1;
 			} else {
-				this.index = this.index - 1;
+				this.activeIndex--;
 			}
-
-			const nextSlide = slides[this.index];
-			nextSlide.isActive = true;
 		},
 		goNextSlide() {
-			const slideActive = slides[this.index];
-			slideActive.isActive = false;
-
-			if (this.index === this.slides.length - 1) {
-				this.index = 0;
+			if (this.activeIndex === this.slides.length - 1) {
+				this.activeIndex = 0;
 			} else {
-				this.index = this.index + 1;
+				this.activeIndex++;
 			}
-
-			const nextSlide = slides[this.index];
-			nextSlide.isActive = true;
 		}
 	}
 });
